0359f9620a194bc9018d7b5c8b70a2744042ee25
paniz520 <momenipaniz049@gmail.com>
2024-11-03 20:50:02 +0330

    alibaba header , one dropdown done

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+ let dropdown1 = document.querySelector(".header-more-dropdown");
+let navToggle = document.querySelector('.nav-toggle'); 
+
+function showMenu1() {
+    if(dropdown1.style.display = 'none'){
+    let dropdown1 = document.querySelector(".header-more-dropdown");
+    dropdown1.style.display = 'block';
+    }
+    console.log('hello');
+}
+
+
+/* 
+    
+    document.body.addEventListener('click', (event) => {
+        if(!dropdown1.contains(event.target)) {
+           dropdown1.style.display = 'none';
+    
+           console.log('hello')
+        }
+    
+    }); */
+    
